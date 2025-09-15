04292d6288280fced117f2a66a5abb6bc6b9b088
Harika3 <90802791+Hardfl@users.noreply.github.com>
2025-09-14 21:41:59 -0400

    Update ConsoleMp3Looper.java

--- ConsoleMp3Looper.java
+++ ConsoleMp3Looper.java
@@ -1,39 +1,30 @@
 import javazoom.jl.player.advanced.AdvancedPlayer;
-import javazoom.jl.player.advanced.PlaybackEvent;
-import javazoom.jl.player.advanced.PlaybackListener;
-
 import java.io.FileInputStream;
 import java.util.Scanner;
 
 public class ConsoleMp3Looper {
+    private static final int FRAMES_PER_SECOND = 38; // approx. for MP3
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        try {
-            // Ask user for file
-            System.out.print("Enter path to MP3 file: ");
-            String filePath = scanner.nextLine();
+        System.out.print("Enter path to MP3 file: ");
+        String filePath = scanner.nextLine();
 
-            // Ask start/end time
-            System.out.print("Enter start time in seconds: ");
-            int startSec = scanner.nextInt();
+        System.out.print("Enter start time (seconds): ");
+        int startSec = scanner.nextInt();
 
-            System.out.print("Enter end time in seconds: ");
-            int endSec = scanner.nextInt();
+        System.out.print("Enter end time (seconds): ");
+        int endSec = scanner.nextInt();
 
-            // Ask loop count
-            System.out.print("How many times to loop: ");
-            int loopCount = scanner.nextInt();
+        System.out.print("How many times to loop? ");
+        int loops = scanner.nextInt();
 
-            // Play the selection in a loop
-            for (int i = 0; i < loopCount; i++) {
-                playSelection(filePath, startSec, endSec);
-            }
-
-            System.out.println("Done looping!");
-        } catch (Exception e) {
-            e.printStackTrace();
+        for (int i = 0; i < loops; i++) {
+            playSelection(filePath, startSec, endSec);
         }
+
+        System.out.println("Finished looping!");
     }
 
     private static void playSelection(String filePath, int startSec, int endSec) {
@@ -41,17 +32,9 @@ public class ConsoleMp3Looper {
             FileInputStream fis = new FileInputStream(filePath);
             AdvancedPlayer player = new AdvancedPlayer(fis);
 
-            int startFrame = startSec * 38; // rough estimate (38 frames/sec for MP3s at 128kbps)
-            int endFrame = endSec * 38;
+            int startFrame = startSec * FRAMES_PER_SECOND;
+            int endFrame = endSec * FRAMES_PER_SECOND;
 
-            player.setPlayBackListener(new PlaybackListener() {
-                @Override
-                public void playbackFinished(PlaybackEvent evt) {
-                    // Can be used for debugging
-                }
-            });
-
-            // Play only between the frames
             player.play(startFrame, endFrame);
 
             player.close();
